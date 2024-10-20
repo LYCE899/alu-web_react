3ceb3c12f58f15a67945766ed70e20e6cd64589c
LYCE899 <a.coulibaly@alustudent.com>
2024-10-20 02:24:36 +0200

    commit

--- react_redux_connectors_and_providers/task_1/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_1/dashboard/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { uiReducer } from './reducers/uiReducer';
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
 
-
-const store = createStore(uiReducer,
-  // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-  composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(uiReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -5,12 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { uiReducer } from './reducers/uiReducer';
 import { Provider } from 'react-redux';
 import { thunk } from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
 
 
-const store = createStore(uiReducer,
-  // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-  composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(uiReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
